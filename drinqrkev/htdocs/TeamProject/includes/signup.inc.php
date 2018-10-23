4d656d47886e97413fd7b234988d23f3504113ce
Kevin <kevinpmbyrne@gmail.com>
2018-10-23 16:30:19 +0100

    Update drinqrkev/htdocs/TeamProject/includes/signup.inc.php

--- drinqrkev/htdocs/TeamProject/includes/signup.inc.php
+++ drinqrkev/htdocs/TeamProject/includes/signup.inc.php
@@ -1,14 +1,20 @@
 <?php
 	include_once 'dbh.inc.php';
+	//mysqli_real_escape_string is used to prevent SQL injection (sanitisation)  -kb
 	
-	$first = $_POST['first'];
-	$last = $_POST['last'];
-	$email = $_POST['email'];
-	$uid = $_POST['uid'];
-	$pwd = $_POST['pwd'];
+	//$conn var = sql connection details found in dbh.inc.php  -kb
+	
+	
+	$first = mysqli_real_escape_string($conn, $_POST['first'];)
+	$last = mysqli_real_escape_string($conn, $_POST['last']);
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$uid = mysqli_real_escape_string($conn, $_POST['uid']);
+	$pwd = mysqli_real_escape_string($conn,$_POST['pwd']);
 	
 	$sql = "INSERT into users (user_first, user_last, user_email, user_uid, user_pwd)
 			VALUES ('$first','$last','$email','$uid','$pwd');";
+	
+	
 	mysqli_query($conn, $sql);
 
 	header("Location: ../index.php?signup=success");
