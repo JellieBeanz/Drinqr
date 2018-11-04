6a5555c1d0eb7bd891f35ebf6a80450a4a25d5d7
JellieBeanz <33373091+JellieBeanz@users.noreply.github.com>
2018-11-04 20:52:04 +0000

    Update drinqrkev/htdocs/TeamProject/includes/dbh.inc.php

--- drinqrkev/htdocs/TeamProject/includes/dbh.inc.php
+++ drinqrkev/htdocs/TeamProject/includes/dbh.inc.php
@@ -3,7 +3,7 @@
 	$dbServername = "localhost";
 	$dbUsername = "root";
 	$dbPassword = "";
-	$dbName = "drinqr";
+	$dbName = "drinqrdb";
 	
 	$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 	
