bc2267383be62e5a17e0291ad58a63c539217543
JellieBeanz <33373091+JellieBeanz@users.noreply.github.com>
2018-11-10 19:35:34 +0000

    Button functionality

--- TeamProject/gallery.php
+++ TeamProject/gallery.php
@@ -36,9 +36,8 @@
 				</div>
 				<div class="collapse navbar-collapse" id="myNavbar">
 					<ul class="nav navbar-nav navbar-right">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">Gallery</a></li>
+						<li><a href="\index.html">Home</a></li>
+						<li class="active"><a href="\Gallery.php">Gallery</a></li>
 						<li><a href="#">About</a></li>
 						<li><a href="#">Contact</a></li>
 					</ul>
