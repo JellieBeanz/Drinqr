8901238ba332c88ddf9b3742581b5c801014e0a4
JellieBeanz <33373091+JellieBeanz@users.noreply.github.com>
2018-11-10 18:57:08 +0000

    Css Fix

--- TeamProject/gallery.php
+++ TeamProject/gallery.php
@@ -9,10 +9,14 @@
 <html>
 
 <head>
-	<meta charset="UTF-8">
+<meta charset="UTF-8">
 	<title>Drinqr</title>
-	<meta name= "viewport" content="width=device-width, initial-scale=1.0">
+	<meta name = "viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="includes/style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body>
@@ -98,8 +102,7 @@
 				</div>
 			</div>
 		</section>
-	</main>	
-	<footer>
+		<footer>
 		<div class="container-fluid text-center">
 				<div class="row">
 					<div class="col-sm-4">
@@ -120,6 +123,8 @@
 			</div>
 	
 	</footer>
+	</main>	
+	
 
 </body>
 
