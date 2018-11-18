d0e55ca9675ce1c06a36cc5c36b3670d8f76cf94
Kev <kevinpmbyrne@gmail.com>
2018-11-18 20:38:11 +0000

    Fixed swipe function
    fixed swipe function animation
    added bar name and price to avatar div
    removed image title from avatar div

---
+++ includes/dbh.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+	$dbServername = "localhost";
+	$dbUsername = "root";
+	$dbPassword = "";
+	$dbName = "drinqr";
+	
+	$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+	
+	
+?>
