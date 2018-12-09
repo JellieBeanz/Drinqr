9106eb5bb900ea3c8cbfb3893996571f535ea276
JellieBeanz <33373091+JellieBeanz@users.noreply.github.com>
2018-12-09 01:48:40 +0000

    new swipe function

---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Drinqr</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900|Cormorant+Garamond:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/jTinder.css">
+	
+  </head>
+
+<body>
+
+	<header> 			<!-- Header which includes the login form -->
+		
+		<nav class ="nav-header-main">
+		<a href="index.php" class="header-brand">DRINQR</a>
+		
+			<ul>
+				<li><a href="about.html">About</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+			</nav>
+			
+			<a href="gallery.php" class="header-cases">DrinqDB</a>
+			
+			<div class="header-login">
+			<?php
+				if (!isset($_SESSION['id'])) {
+					echo '<form action ="includes/login.php" method ="post">
+						<input type = "text" name = "mailuid" placeholder ="Username">
+						<input type = "password" name = "pwd" placeholder ="Password">
+						<button type = "submit" name = "login-submit">Login</button>
+					</form>
+					<a href ="signup.php" class="header-signup">Signup</a>';
+				}
+				else if (isset($_SESSION['id'])) {
+					echo '	<form action ="includes/logout.php" method ="post">
+							<button type = "submit" name = "logout-submit">Logout</button>
+							</form>';			
+				}
+			?>
+			</div>
+		</nav>
+
---
+++ signup.php
@@ -0,0 +1,66 @@
+<?php
+  require "header.php";
+?>
+    </header>
+	<main>
+      <div class="wrapper-main">
+        <section class="section-default">
+          <h1>Signup</h1>
+          <?php
+          if (isset($_GET["error"])) {
+            if ($_GET["error"] == "emptyfields") {
+              echo '<p class="signuperror">Fill in all fields!</p>';
+            }
+            else if ($_GET["error"] == "invaliduidmail") {
+              echo '<p class="signuperror">Invalid username and e-mail!</p>';
+            }
+            else if ($_GET["error"] == "invaliduid") {
+              echo '<p class="signuperror">Invalid username!</p>';
+            }
+            else if ($_GET["error"] == "invalidmail") {
+              echo '<p class="signuperror">Invalid e-mail!</p>';
+            }
+            else if ($_GET["error"] == "passwordcheck") {
+              echo '<p class="signuperror">Your passwords do not match!</p>';
+            }
+            else if ($_GET["error"] == "usertaken") {
+              echo '<p class="signuperror">Username is already taken!</p>';
+            }
+          }
+          else if (isset($_GET["signup"])) {
+            if ($_GET["signup"] == "success") {
+              echo '<p class="signupsuccess">Signup successful!</p>';
+            }
+          }
+          ?>
+		  
+          <form class="form-signup" action="includes/signup.inc.php" method="post"> <!-- signup form using post method -->
+          <?php
+
+            if (!empty($_GET["uid"])) {	//checks if 
+              echo '<input type="text" name="uid" placeholder="Username" value="'.$_GET["uid"].'">'; 
+            }
+            else {
+              echo '<input type="text" name="uid" placeholder="Username">';
+            }
+
+     
+            if (!empty($_GET["mail"])) {
+              echo '<input type="text" name="mail" placeholder="E-mail" value="'.$_GET["mail"].'">';
+            }
+            else {
+              echo '<input type="text" name="mail" placeholder="E-mail">';
+            }
+            ?>
+            <input type="password" name="pwd" placeholder="Password">
+            <input type="password" name="pwd-repeat" placeholder="Repeat password">
+            <button type="submit" name="signup-submit">Signup</button>
+          </form>
+
+        </section>
+      </div>
+    </main>
+
+<?php
+  require "footer.php";
+?>
