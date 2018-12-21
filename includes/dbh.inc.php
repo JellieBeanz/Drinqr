ff97047d59459d474d7a453c76ce22e8a3002de6
JellieBeanz <33373091+JellieBeanz@users.noreply.github.com>
2018-12-21 10:04:03 +0000

    db inc.
    for azure deployment

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,14 +1,16 @@
-<?php
 
-	$dbServername = "shanesproj.database.windows.net";
-	$dbUsername = "x15048209";
-	$dbPassword = "Jwfn3262";
-	$dbName = "Drinqr";
-	
-	$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-	
-	if (!$conn){
-		
-		die("Connection failed: ".mysqli_connect_error()); //kills connection if it fails and outputs error message
-	}
-?>
+
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:shanesproj.database.windows.net,1433; Database = Drinqr", "x15048209", "Jwfn3262");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "x15048209@shanesproj", "pwd" => "Jwfn3262", "Database" => "Drinqr", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:shanesproj.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
